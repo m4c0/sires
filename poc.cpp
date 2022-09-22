365147c39895aade5d4659f7474b06f7136eca59
Eduardo Costa <m4c0@users.noreply.github.com>
2022-09-22 19:06:58 +0100

    osx: first working version

--- poc.cpp
+++ poc.cpp
@@ -1,5 +1,13 @@
+#include "sires.hpp"
+
 #include <iostream>
 
 int main() {
+  std::unique_ptr<std::streambuf> buf { sires::open("poc", "txt") };
+  if (!buf) {
+    std::cout << "Failed to read test file\n";
+    return 1;
+  }
+
   std::cout << "OK\n";
 }
