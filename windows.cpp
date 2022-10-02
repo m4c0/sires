1b6356a8429cb8198220daea3c85599d274202f8
Eduardo Costa <m4c0@users.noreply.github.com>
2022-10-02 10:37:33 +0100

    windows support

---
+++ windows.cpp
@@ -0,0 +1,23 @@
+#include "sires.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <tchar.h>
+#include <windows.h>
+
+inline auto exe_path() noexcept {
+  TCHAR exepath[MAX_PATH + 1];
+
+  if (GetModuleFileName(0, exepath, MAX_PATH + 1) == 0) {
+    std::terminate();
+  }
+
+  return std::filesystem::path { exepath };
+}
+
+std::streambuf * sires::open(const char * name, const char * ext) {
+  const auto path = exe_path().replace_filename(name).replace_extension(ext);
+  auto * res = new std::filebuf();
+  if (!res->open(path, std::ios::binary | std::ios::in)) return nullptr;
+  return res;
+}
