2062e7383e1ba4d9eb679e04e728368f4e12d6cf
Eduardo Costa <m4c0@users.noreply.github.com>
2022-10-04 10:25:17 +0100

    wasm: concat name and ext

--- wasm.cpp
+++ wasm.cpp
@@ -1,9 +1,11 @@
 #include "sires.hpp"
 
 #include <fstream>
+#include <string>
 
 std::streambuf * sires::open(const char * name, const char * ext) {
+  auto fname = std::string { name } + "." + ext;
   auto * res = new std::filebuf();
-  res->open(name, std::ios::binary | std::ios::in);
+  res->open(fname, std::ios::binary | std::ios::in);
   return res;
 }
