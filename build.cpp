991c7d697d01d8a7b1c8f0f29a14c3d7b6fd8582
Eduardo Costa <m4c0@users.noreply.github.com>
2023-02-08 15:00:27 +0000

    split build include + extra mm for objc

--- build.cpp
+++ build.cpp
@@ -1,50 +1,12 @@
-#include "../hai/build.hpp"
-#include "../jute/build.hpp"
-#include "../missingno/build.hpp"
-#include "../yoyo/build.hpp"
-#include "ecow.hpp"
+#include "build.hpp"
 
 using namespace ecow;
 
 int main(int argc, char ** argv) {
+  auto m = sires();
+
   seq all { "all" };
-
-  auto yo = unit::create<per_feat<seq>>("yo");
-  yo->for_feature(posix).add_ref(yoyo_libc());
-  yo->for_feature(android_ndk).add_ref(yoyo());
-  yo->for_feature(webassembly).add_ref(yoyo());
-
-  auto m = all.add_unit<per_feat<mod>>("sires");
-  m->add_wsdep("hai", hai());
-  m->add_wsdep("jute", jute());
-  m->add_wsdep("missingno", missingno());
-  m->add_wsdep("yoyo", yo);
-  m->add_feat<js>()->set(
-      "sires_open",
-      R"((nm_ptr, len, ref_ptr) => {
-  const name = new TextDecoder().decode(new Uint8Array(ecow_buffer, nm_ptr, len));
-  ecow_globals.sires = ecow_globals.sires || [];
-  const ref = ecow_globals.sires.length;
-  new Uint32Array(ecow_buffer, ref_ptr, 1)[0] = ref;
-  ecow_globals.sires.push(null);
-  fetch(name)
-    .then(response => response.arrayBuffer())
-    .then(bytes => new Uint8Array(bytes))
-    .then(array => ecow_globals.sires[ref] = array)
-    .then(_ => window.dispatchEvent(new CustomEvent('sires_opened', { detail: ref })));
-})");
-  m->add_feat<js>()->set("sires_fsize", R"((ref) => {
-  return ecow_globals.sires[ref].length;
-})");
-  m->add_feat<js>()->set("sires_read", R"((ref, offs, ptr, len) => {
-  const buf = ecow_globals.sires[ref].subarray(offs, len);
-  new Uint8Array(ecow_buffer, ptr, len).set(buf);
-})");
-
-  m->for_feature(android_ndk).add_impl("android");
-  m->for_feature(objective_c).add_impl("apple");
-  m->for_feature(webassembly).add_impl("wasm");
-  m->for_feature(windows_api).add_impl("windows");
+  all.add_ref(m);
 
   auto poc = all.add_unit<app>("sires-poc");
   poc->add_feat<js>()->set("poc_open", "");
