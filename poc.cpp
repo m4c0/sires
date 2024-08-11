1002d344155cc18b545b14bac3f607fad3867f29
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-11 14:33:14 +0100

    poc: tracing

--- poc.cpp
+++ poc.cpp
@@ -9,17 +9,18 @@ int main() {
 
   char buf[5] {};
   return sires::open("poc.txt"_s)
+      .trace("opening file")
       .fmap([&](auto && rdr) {
         while (!rdr.ready()) {
           // sleep, update ui, etc
         }
-        return rdr.read(buf, 4);
+        return rdr.read(buf, 4).trace("reading file");
       })
       .map([&] {
         silog::log(silog::info, "Got: (%s)", buf);
       })
       .fmap([] {
-        return sires::stat("poc.txt");
+        return sires::stat("poc.txt").trace("stating file");
       })
       .map([](auto ts) {
         silog::log(silog::info, "Mod. timestamp: %llu", ts);
