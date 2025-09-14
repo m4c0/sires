740355fd0d2c7ecb6a95111212e71e62c47260e6
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-14 15:59:34 +0100

    introducing non-bundle test

---
+++ poc-tool.cpp
@@ -0,0 +1,18 @@
+#pragma leco tool
+
+import jute;
+import hai;
+import print;
+import sires;
+
+int main() {
+  putan("File at:", sires::real_path_name("poc.txt"));
+
+  sires::read("poc.txt", nullptr, [](void *, hai::cstr & data) {
+    putfn("Got %d bytes [%s]", data.size(), data.begin());
+  });
+
+  sires::read("no-file", nullptr, [](void *, hai::cstr & data) {
+    errln("Unreachable");
+  });
+}
