042d55ec6e6f0e3239fef522dbef9fc18ffcf08e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-09-14 13:49:51 +0100

    "negative" example

--- poc.cpp
+++ poc.cpp
@@ -8,7 +8,13 @@ import sires;
 
 int main() {
   silog::log(silog::debug, "File at: %s", sires::real_path_name("poc.txt").begin());
+
   sires::read("poc.txt", nullptr, [](void *, hai::cstr & data) {
     silog::log(silog::debug, "Got %d bytes [%s]", data.size(), data.begin());
+  });
+
+  sires::read("no-file", nullptr, [](void *, hai::cstr & data) {
+    silog::log(silog::error, "Unreachable");
+    throw 0;
   });
 }
